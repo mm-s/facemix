fdbe64ab4348b78833bc1af0d2048899711bdac1
Marcos Mayorga <mm@mm-studios.com>
2014-12-06 16:57:41 +0000

    x

--- www/index.php
+++ www/index.php
@@ -1,3 +1,7 @@
+<?
+$ip=$_SERVER["REMOTE_ADDR"];
+?> 
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,26 +10,45 @@
 
 <!-- Include the PubNub Library -->
 <script src="https://cdn.pubnub.com/pubnub.min.js"></script>
+<script src=”http://pubnub.github.io/webrtc/webrtc-beta-pubnub.js”></script>
 
 <!-- Instantiate PubNub -->
 <script type="text/javascript">
-var PUBNUB_facemix = PUBNUB.init({
+var pubnub = PUBNUB.init({
 publish_key: 'pub-c-c25d73f6-1b1e-4b49-813f-f5eda5ac120e',
-subscribe_key: 'sub-c-496b23ee-7d21-11e4-812f-02ee2ddab7fe'
+subscribe_key: 'sub-c-496b23ee-7d21-11e4-812f-02ee2ddab7fe',
+uuid: '<?=$ip?>'
 });
 
-PUBNUB_facemix.subscribe({
-channel: 'demo_tutorial',
+pubnub.subscribe({
+channel: 'facemix',
 message: function(m){console.log(m);},
 connect: publish
+
 }); 
 
 function publish() {
-    PUBNUB_facemix.publish({
-    channel: 'demo_tutorial',
-    message: {"text":"Hey!"}
-});
+    pubnub.here_now({
+        channel: 'facemix',
+        callback: function(m){console.log(m);}
+    })
+
+
+
+    pubnub.publish({
+    channel: 'facemix',
+    message: {"text":"Hey joe!"}
+    });
+    
+    
+    pubnub.here_now({
+        channel: 'facemix',
+        callback: function(m){console.log(m);}
+    })
+    
 } 
+
+
 
 
 </script> 
@@ -35,5 +58,6 @@ function publish() {
 </head>
 <body>
 <h2>facemix</h2>
+IP <?=$ip?> 
 </body>
 </html>
